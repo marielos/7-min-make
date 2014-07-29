35aed5f0c112c3543f1ce2ec24fc6979b5bcd886
marielos <msanson@stanford.edu>
2014-07-29 14:47:02 -0600

    removed multi part from the form

--- app/assets/javascripts/photos.js
+++ app/assets/javascripts/photos.js
@@ -4,7 +4,6 @@ function readURL(input) {
         reader.onload = function (e) {
             $('#blah').attr('src', e.target.result);
         }
-
         reader.readAsDataURL(input.files[0]);
     }
 }
