5f31b080bcae87138493d37b3c07da1e0c87be43
marielos <msanson@stanford.edu>
2014-07-29 22:04:26 -0600

    added compress true to config production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -26,8 +26,10 @@ Maker::Application.configure do
 
   #config.assets.js_compressor = :uglifier
   #config.assets.css_compressor = :sass
+  config.assets.compress = true
   config.assets.js_compressor = :uglify
   config.assets.css_compressor = :yui
+
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
